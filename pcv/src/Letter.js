e2ebe805860867943d723a56689317159759b6d9
Kaitlyn Huynh <kahu1074@colorado.edu>
2022-09-26 12:01:48 -0600

    fixed nesting error

--- pcv/src/Letter.js
+++ pcv/src/Letter.js
@@ -39,7 +39,7 @@ function Letter(){
                     While others flocked to Instagram or swore by Tumblr I've always been a Pinterest girl deep down. 
                     Pinterest is <b>the</b> go-to platform for inspiration, no one does it like you guys. Anytime I'm
                     looking for something new to try or narrowing down an idea, I open your app and browse for hours (not literally...
-                    <p className="letter-paragraph-small">okay sometimes</p> ). The point is, I'm passionate about your product and would love 
+                    <em>okay sometimes</em> ). The point is, I'm passionate about your product and would love 
                     the opportunity to work on something I've used so much in my life. With <Tippy theme={"light"} content = {<img className = "tippy-image" style = {{width: '15em',}} src = {pin} />} ><button className ='button-link'>pins</button></Tippy> being such a huge
                     part of your product, I hope you don't mind if I include a few of my own to showcase my personality. 
                     Hopefully it'll give a quick  <button id = "pin" className="button-link" onClick = {imageOut}>peek</button> 
@@ -51,7 +51,7 @@ function Letter(){
                     and technology and I've always dreamed of working for a company who specializes in the intersection. 
                     This relationship is something that I feel is still severly underappreciated, but Pinterest has been way ahead 
                     of the game and I'd love to bring my own understanding and skillset to your company. I've been <Tippy theme = {"light"} content = {<img className = "tippy-image" style = {{width: '20em',}} src = {watching} />} ><button className = 'button-link'>stalking</button></Tippy> the careers page for a bit. When the New Grad position popped up on
-                    your Careers page, I've never clicked on something <Tippy theme = {"light"} content = {<img className = "tippy-image" style = {{width: '20em',}} src = {breakWall} />} ><button className = 'button-link'>faster</button></Tippy> 
+                    yourCareers page, I've never clicked on something <Tippy theme = {"light"} content = {<img className = "tippy-image" style = {{width: '20em',}} src = {breakWall} />} ><button className = 'button-link'>faster</button></Tippy> 
                     in my life. As far as what I bring to the <Tippy theme = {"light"} content = {<img className = "tippy-image" style = {{width: '20em'}} src = {table} />} ><button className = 'button-link'>table</button></Tippy>, 
                     Creative Technology & Design has provided me with a <Tippy theme = {"light"} content = {<img className = "tippy-image" style = {{width: '22em',}} src = {multi} />} ><button className = 'button-link'>multitude</button></Tippy> of skills to use at my disposal. However, what I specialize in is <Tippy theme = {"light"} content = {<img className = "tippy-image" style = {{width: '20em',}} src = {css} />} ><button className = 'button-link'>front-end development.</button></Tippy> 
                     Throughout personal studies, jobs, and school I've built multiple applications and websites in trusty
